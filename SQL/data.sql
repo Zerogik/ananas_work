0e424023fe74fe7e0a6a3b8d6c58bb03b58aa0d5
Iskrov Alexey <iskrovalexey@gmail.com>
2021-08-29 23:23:47 +0300

    трыньк 3

--- SQL/data.sql
+++ SQL/data.sql
@@ -10,3 +10,8 @@ VALUES (1, 'admin'),
        (3, 'engineer');
 INSERT INTO staff (id, st_name, username, password, contact, email, role_id, activity, salary_id)
 VALUES (1, 'Алексей Искров', 'zerogik', '150278', '8(963)618-40-45', 'iskrovalexey@gmail.com', 1, true, 4);
+INSERT INTO clients (cl_name, contacts, email, note)
+VALUES ('zeslone', '8(963)618-40-45', 'rogik@inbox.ru', 'злобные упыри'),
+       ('Этери Бериашвили', '8(925)002-03-00', 'eteriberi@gmail.com', 'певица, голос 2, нука все вместе, джаз паркинг, препод хора Ларисы долиной'),
+       ('репер коля', '03', 'тупой не имеет почты', 'shvine');
+
