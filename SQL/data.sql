410b2bd1c2b51d0f9a3debdfe95a7163f58c5dad
Iskrov Alexey <iskrovalexey@gmail.com>
2021-08-31 00:43:33 +0300

    1234567

--- SQL/data.sql
+++ SQL/data.sql
@@ -1,17 +1,18 @@
-USE orders;
-INSERT INTO salary_type (id, sl_name)
+USE studio;
+INSERT INTO salary_type (id, name)
 VALUES (1, 'sal20'),
        (2, 'sal30'),
        (3, 'sal40'),
        (4, 'sal45');
-INSERT INTO role (id, rl_name)
+INSERT INTO role (id, name)
 VALUES (1, 'admin'),
        (2, 'director'),
        (3, 'engineer');
-INSERT INTO staff (id, st_name, username, password, contact, email, role_id, activity, salary_id)
+INSERT INTO staff (id, name, username, password, contact, email, role_id, activity, salary_id)
 VALUES (1, 'Алексей Искров', 'zerogik', '150278', '8(963)618-40-45', 'iskrovalexey@gmail.com', 1, true, 4);
-INSERT INTO clients (cl_name, contacts, email, note)
+INSERT INTO clients (name, contacts, email, note)
 VALUES ('zeslone', '8(963)618-40-45', 'rogik@inbox.ru', 'злобные упыри'),
-       ('Этери Бериашвили', '8(925)002-03-00', 'eteriberi@gmail.com', 'певица, голос 2, нука все вместе, джаз паркинг, препод хора Ларисы долиной'),
+       ('Этери Бериашвили', '8(925)002-03-00', 'eteriberi@gmail.com',
+        'певица, голос 2, нука все вместе, джаз паркинг, препод хора Ларисы долиной'),
        ('репер коля', '03', 'тупой не имеет почты', 'shvine');
 
---
+++ SQL/orders_dbALT.sql
@@ -0,0 +1,79 @@
+CREATE SCHEMA studio;
+USE studio;
+
+CREATE TABLE role
+(
+    id   INT PRIMARY KEY NOT NULL,
+    name VARCHAR(20)
+);
+
+CREATE TABLE salary_type
+(
+    id   INT PRIMARY KEY NOT NULL,
+    name VARCHAR(20)
+);
+
+CREATE TABLE staff
+(
+    id        INT PRIMARY KEY NOT NULL,
+    name      VARCHAR(50)     NOT NULL,
+    username  VARCHAR(20)     NOT NULL,
+    password  VARCHAR(255)    NOT NULL,
+    contact   VARCHAR(30)     NOT NULL,
+    email     VARCHAR(100)    NOT NULL,
+    role_id   INT             NOT NULL,
+    activity  BOOLEAN,
+    salary_id INT             NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id)
+        REFERENCES role (id),
+    CONSTRAINT fk_salary FOREIGN KEY (salary_id)
+        REFERENCES salary_type (id)
+);
+
+CREATE TABLE clients
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    name     VARCHAR(50) NOT NULL,
+    contacts VARCHAR(30) NOT NULL,
+    email    VARCHAR(50),
+    note     VARCHAR(250)
+);
+CREATE TABLE projects
+(
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    completed  BOOLEAN  NOT NULL,
+    client_id INT NOT NULL,
+    CONSTRAINT fk_project_client FOREIGN KEY (client_id) REFERENCES clients (id)
+);
+CREATE TABLE price
+(
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    cost INT          NOT NULL
+);
+
+CREATE TABLE sessions
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    client_id  INT      NOT NULL,
+    staff_id   INT      NOT NULL,
+    price_id   INT      NOT NULL,
+    project_id INT      NOT NULL,
+    note       VARCHAR(255),
+    start_time DATETIME NOT NULL,
+    duration   INT      NOT NULL,
+    paid       BOOLEAN  NOT NULL,
+    CONSTRAINT fk_staff FOREIGN KEY (staff_id) REFERENCES staff (id),
+    CONSTRAINT fk_cl FOREIGN KEY (client_id) REFERENCES clients (id),
+    CONSTRAINT fk_price FOREIGN KEY (price_id) REFERENCES price (id),
+    CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES projects (id)
+);
+
+CREATE TABLE buhgalters_book (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    paydate DATETIME DEFAULT CURRENT_TIMESTAMP(),
+    project_id INT NOT NULL,
+    summ INT
+)
+
