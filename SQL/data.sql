2e50f2a2ec0edad9ac3343c329c262ed559ffd43
Iskrov Alexey <iskrovalexey@gmail.com>
2021-08-31 15:49:39 +0300

    12345678

--- SQL/data.sql
+++ SQL/data.sql
@@ -15,4 +15,23 @@ VALUES ('zeslone', '8(963)618-40-45', 'rogik@inbox.ru', 'злобные упыр
        ('Этери Бериашвили', '8(925)002-03-00', 'eteriberi@gmail.com',
         'певица, голос 2, нука все вместе, джаз паркинг, препод хора Ларисы долиной'),
        ('репер коля', '03', 'тупой не имеет почты', 'shvine');
-
+INSERT INTO price (name, cost)
+VALUES ('record hour', 1200),
+       ('mix vox+minus', 3000),
+       ('rec + mix vox & minus', 6000),
+       ('mix multitrack', 10000),
+       ('aranj', 15000),
+       ('aranj +text', 18000),
+       ('aaranj for key', 22000);
+INSERT INTO staff (id, name, username, password, contact, email, role_id, activity, salary_id)
+VALUES (2,'Чистяков Онотоле', 'админсись', 'qwerty', '89520080007', 'alacurdin@gmail.com', 2, 1, 4),
+       (3, 'Gonza Bartochkov', 'fooflo', 'ytrewq', '89216676869', 'gonza@mai.ru', 3, 1, 2);
+INSERT INTO staff (id, name, username, password, contact, email, role_id, activity, salary_id)
+VALUES (4, 'Jirobas Inna', 'fat_fat', '123', '89268768786', 'faat@mail.ru', 3, 1, 1)
+INSERT INTO projects (name, completed, client_id)
+VALUES ('voron', 0, 2),
+       ('pro konya', 1, 2),
+       ('гангстер прыщ', 0, 3),
+       ('crack', 1, 3),
+       ('j-j-j', 0, 1),
+       ('r-r-r', 1, 1);
