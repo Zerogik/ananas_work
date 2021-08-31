010872e1dd83561f26fece979283d0143bc121dc
Iskrov Alexey <iskrovalexey@gmail.com>
2021-08-31 19:34:45 +0300

    12345678

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-
-        System.out.println(a + b);
+        int x;
+        String a;
+        String b;
+        String c,d;
+        a = ",kimoR";
+        b = " ";
+        c = " et ner";
+        d = "h v";
+        String k;
+        String h;
+        h = " ohu ";
+        k = a + b + c + d + h;
+        String str3 = k;
+        String[] words = new StringBuilder(str3).reverse().toString().split(" ");
+        StringBuilder newStr = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) newStr.append(words[i]).append(" ");
+        System.out.println(newStr);
     }
+
 }
